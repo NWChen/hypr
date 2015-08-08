014a4ee0a769a9328a5a751e64fa85f4f17e8bcf
NWChen <neilwchen@gmail.com>
2015-08-08 14:08:54 -0400

    get all tabs

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+function getAllUrls() {
+	var tab_list = ["asdfsdafdsa"];
+	chrome.tabs.query({}, function(tabs) {     
+	  tabs.forEach(function(tab){
+  		tab_list.push(tab.url);
+	  });
+	});
+	alert(tab_list);
+	return tab_list;
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		getAllUrls();
+});		
+
+/*
 function alertshit(tab) {
-	if(tab)
-		alert(tab.url);
+	if(tab) {
+		var tab_list = ["asdfsdafdsa"];
+		chrome.tabs.query({}, function(tabs){     
+		  console.log("\n/////////////////////\n");
+		  tabs.forEach(function(tab){
+	  		tab_list.push(tab.url);
+		  });
+		});
+		alert(tab_list);
+	}
 };
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     alertshit(tab);
 });		
 
+
+*/
