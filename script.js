5f10f95e866dde4aec1a1a16455225c70ca6c5f2
NWChen <neilwchen@gmail.com>
2015-08-08 14:48:31 -0400

    all tab ids

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
-function getAllUrls() {
+function getAllTabsWithProperty(key) {
 	var tab_list = ["asdf"];
 	chrome.tabs.query({}, function(tabs) {
 		tabs.forEach(function(tab){
-	  	tab_list.push(tab.url);
+	  	tab_list.push(tab[key]);
 		});
 	});
 	return tab_list;
 };
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(tab) console.log(getAllUrls());
+	getAllTabsWithProperty('id').forEach(function(id) {
+		//chrome.tabs.sendRequest(id, {})
+	});
 });		
-
-/*
-function alertshit(tab) {
-	if(tab) {
-		var tab_list = ["asdfsdafdsa"];
-		chrome.tabs.query({}, function(tabs){     
-		  console.log("\n/////////////////////\n");
-		  tabs.forEach(function(tab){
-	  		tab_list.push(tab.url);
-		  });
-		});
-		alert(tab_list);
-	}
-};
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    alertshit(tab);
-});		
-
-
-*/
