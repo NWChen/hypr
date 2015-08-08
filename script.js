05b8c160bb6858c26911f5f27d2715b283139840
NWChen <neilwchen@gmail.com>
2015-08-08 16:41:54 -0400

    ??

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function realism(){
-//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	//if(tab) {
+$("#logoutOfApps").click(function() {
+	console.log("HELLYES");
+	gothru();
+});
 
-		//get all tabs
-		chrome.tabs.query({}, function(tabs) {
-			tabs.forEach(function(tab){
-		  	chrome.tabs.update(tab.id, {selected:true});
+function gothru(){
 
-		  	//get all tab DOMs
-		  	chrome.tabs.executeScript({
-		  		code: "document.getElementsByTagName('a')[0].click()"
-		  	}, function(id) {
-		  		console.log(id);
-		  	});
-			});
+	//get all tabs
+	chrome.tabs.query({}, function(tabs) {
+		tabs.forEach(function(tab){
+	  	chrome.tabs.update(tab.id, {selected:true});
+
+	  	//get all tab DOMs
+	  	chrome.tabs.executeScript({
+	  		code: "document.getElementsByTagName('a')[0].click()"
+	  	}, function(id) {
+	  		console.log(id);
+	  	});
 		});
-	}
-//});		
+	});
+}		
