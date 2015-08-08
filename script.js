d7fce263072be151ffd7e0545e648ca37f048bf7
NWChen <neilwchen@gmail.com>
2015-08-08 15:32:33 -0400

    iterate through all tabs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//doesnt do anything
 function getAllTabsWithProperty(key) {
-	var tab_list = ["asdf"];
+	var tab_list = [];
 	chrome.tabs.query({}, function(tabs) {
 		tabs.forEach(function(tab){
 	  	tab_list.push(tab[key]);
@@ -9,7 +10,16 @@ function getAllTabsWithProperty(key) {
 };
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	getAllTabsWithProperty('id').forEach(function(id) {
-		//chrome.tabs.sendRequest(id, {})
-	});
+	if(tab) {
+		chrome.tabs.query({}, function(tabs) {
+			tabs.forEach(function(tab){
+		  	chrome.tabs.update(tab.id, {selected:true});
+		  	chrome.tabs.executeScript({
+		  		code: "document.documentElement.innerHTML"
+		  	}, function(html) {
+		  		console.log(html);
+		  	});
+			});
+		});
+	}
 });		
